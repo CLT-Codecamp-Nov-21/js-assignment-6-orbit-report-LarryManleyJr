a8398d954aacc5e1ff8e6859e85af29a9028e2f9
Notorious Nerd <NotoriousNerd@The-11-Creative.local>
2022-01-23 14:06:34 -0500

    completion

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -15,10 +15,11 @@ export class Satellite {
    }
 	
 	isSpaceDebris(): boolean {
- 		this.type ==="Space Debris";
- 			return true;
+ 		return this.type ==="Space Debris";
+ 			
     }
 
 }
+console.log(Satellite)
 
 // TODO 3a: fix isSpaceDebris check
